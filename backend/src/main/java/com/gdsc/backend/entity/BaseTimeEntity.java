ff8e37eb747d46f139bc328bd4469205a26ba0b6
jiy00nn <wldbs204@gmail.com>
2022-02-12 02:48:19 +0900

    DAS-52 feature:Create Diary Schema

--- backend/src/main/java/com/gdsc/backend/entity/BaseTimeEntity.java
+++ backend/src/main/java/com/gdsc/backend/entity/BaseTimeEntity.java
@@ -1,10 +1,12 @@
 package com.gdsc.backend.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDate;
@@ -13,9 +15,13 @@ import java.time.LocalDate;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
+    @Schema(description = "생성 날짜")
     @CreatedDate
+    @Column(name="created_date")
     private LocalDate createdDate;
 
+    @Schema(description = "수정 날짜")
     @LastModifiedDate
+    @Column(name="modified_date")
     private LocalDate modifiedDate;
 }
