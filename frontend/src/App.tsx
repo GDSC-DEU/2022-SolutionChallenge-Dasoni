14e6207b8bfc63064c96a2f70de6b7318ed6e9ef
jeonghye-choi <ppllhm.0@gmail.com>
2022-02-12 21:54:28 +0900

    DAS-57 feat: add router for diary page(main page)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
 import * as React from 'react';
 import Diary from './pages/Diary';
+import Layout from './layouts/Layout';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <Diary />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Diary />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- frontend/src/pages/Diary/index.tsx
+++ frontend/src/pages/Diary/index.tsx
@@ -1,7 +1,18 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 
 function Diary() {
-  return <>Diary Page</>;
+  const [dummyDiaries, setdummyDiaries] = useState([]);
+  return (
+    <>
+      <h2>Diary Page</h2>
+      <ol>
+        {dummyDiaries.map((diary: Diary) => (
+          <li key={diary.diary_id}>{diary.title}</li>
+        ))}
+      </ol>
+    </>
+  );
 }
 
 export default Diary;
