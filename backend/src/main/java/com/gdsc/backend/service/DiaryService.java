fb950703a82e21480b7c81eb9788f3f178f9efdb
d0ri123 <lsh81550@gmail.com>
2022-02-13 20:31:57 +0900

    DAS-64 docs: Add search diary method

--- backend/src/main/java/com/gdsc/backend/service/DiaryService.java
+++ backend/src/main/java/com/gdsc/backend/service/DiaryService.java
@@ -2,11 +2,13 @@ package com.gdsc.backend.service;
 
 import com.gdsc.backend.entity.Diary;
 import com.gdsc.backend.http.request.DiaryRequest;
+import com.gdsc.backend.http.response.DiaryResponse;
 import com.gdsc.backend.repository.DiaryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
@@ -15,12 +17,18 @@ public class DiaryService {
     private final DiaryRepository diaryRepository;
 
     @Autowired
-    public DiaryService(DiaryRepository diaryRepository) { this.diaryRepository=diaryRepository; }
+    public DiaryService(DiaryRepository diaryRepository) {this.diaryRepository=diaryRepository; }
 
     public Page<Diary> findDiaries(Pageable pageable){
         return diaryRepository.findAll(pageable);
     }
 
+    @Transactional
+    public Diary findById(Long id){
+        Diary entity = diaryRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다 id=" + id));
+        return new Diary();
+    }
     public Diary save(DiaryRequest diaryRequest) {
         return diaryRepository.save(
                 Diary.builder()
@@ -33,4 +41,6 @@ public class DiaryService {
     public void deleteById(UUID idx) {
         diaryRepository.deleteById(idx);
     }
+
+
 }
