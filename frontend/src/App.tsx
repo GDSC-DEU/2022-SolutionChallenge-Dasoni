02da46289f901ff7208f322a3523bba0ec91b3e1
jeonghye-choi <ppllhm.0@gmail.com>
2022-02-19 09:53:41 +0900

    feat: add `DiaryPost` form applying atomic `DiaryInput` component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import MainLayout from "./layouts/MainLayout";
 import Diary from "./pages/Diary";
+import DiaryPost from "./pages/DiaryPost";
 import GlobalStyle from "./styles/GlobalStyle";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Diary />} />
+          <Route path="post" element={<DiaryPost />} />
         </Route>
       </Routes>
     </BrowserRouter>
