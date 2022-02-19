bf2877c96744713e43ed5c85323c80e414bc782b
jeonghye-choi <ppllhm.0@gmail.com>
2022-02-19 09:40:51 +0900

    feat(atomic): add `WriteButton` atomic component

---
+++ frontend/src/components/atoms/buttons/WriteButton/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const WriteButtonWrapper = styled.div`
+  position: fixed;
+  right: 16px;
+  bottom: 20px;
+
+  width: 56px;
+  height: 56px;
+  background: #9b9b9b;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  cursor: pointer;
+  box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.14);
+`;
