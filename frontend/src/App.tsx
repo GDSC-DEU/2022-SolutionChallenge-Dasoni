098b258d17fdfcab66d013d2d2d4b029658718a0
jeonghye-choi <ppllhm.0@gmail.com>
2022-02-19 09:38:46 +0900

    feat: apply global styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Layout from "./layouts/Layout";
 import Diary from "./pages/Diary";
+import GlobalStyle from "./styles/GlobalStyle";
 
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Diary />} />
