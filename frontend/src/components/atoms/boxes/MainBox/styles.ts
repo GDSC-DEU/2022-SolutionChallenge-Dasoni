f27717c9279c8d6700f09f755842cb49fd4364b4
jeonghye-choi <ppllhm.0@gmail.com>
2022-02-28 15:14:17 +0900

    style: update `drop-shadow` styles

--- frontend/src/components/atoms/boxes/MainBox/styles.ts
+++ frontend/src/components/atoms/boxes/MainBox/styles.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const MainBoxWrap = styled.div`
   background-color: #f7f7f7;
   border-radius: 10px;
-  drop-shadow: 0px 0px 4px rgba(0, 0, 0, 0.48);
+  filter: drop-shadow(0px 0px 1px rgba(0, 0, 0, 0.48))
+    drop-shadow(0px 0px 4px rgba(0, 0, 0, 0.12));
   padding: 10px 20px;
 `;
