69d90072eec6e0261af1d9d11d39cb0359111ac0
jeonghye-choi <ppllhm.0@gmail.com>
2022-03-04 17:42:10 +0900

    feat: add click event and Link; `nav bar`

--- frontend/src/layouts/MainLayout/index.tsx
+++ frontend/src/layouts/MainLayout/index.tsx
@@ -1,31 +1,60 @@
 import { Header } from "../../components/atoms/wrapper/Header";
 import * as React from "react";
+import { useState, useCallback } from "react";
 import { Outlet } from "react-router";
 import { Link } from "react-router-dom";
 
 import { ButtonBox, Main, Nav } from "./styles";
 
 function MainLayout() {
+  const [currentPage, setCurrentPage] = useState("Diary");
+
+  const onNavItemClicked = useCallback(
+    (page: string) => {
+      setCurrentPage(page);
+    },
+    [currentPage]
+  );
+
   return (
     <>
       <Header>
-        <div>
+        <div className="logo">
           <Link to="/">DASONI</Link>
         </div>
         <ButtonBox>
-          <span>Search</span>
-          <span>hamberger</span>
+          <span className="search"></span>
+          <span className="hamberger-menu"></span>
         </ButtonBox>
       </Header>
       <Nav>
-        <Link to="">
-          <div className="nav-item stayed">Diary</div>
+        <Link to="/">
+          <div
+            className={
+              currentPage === "Diary" ? "nav-item current" : "nav-item"
+            }
+            onClick={() => onNavItemClicked("Diary")}
+          >
+            Diary
+          </div>
         </Link>
-        <Link to="">
-          <div className="nav-item">Feed</div>
+        <Link to="/feed">
+          <div
+            className={currentPage === "Feed" ? "nav-item current" : "nav-item"}
+            onClick={() => onNavItemClicked("Feed")}
+          >
+            Feed
+          </div>
         </Link>
-        <Link to="">
-          <div className="nav-item">Calender</div>
+        <Link to="/statistics">
+          <div
+            className={
+              currentPage === "Statistics" ? "nav-item current" : "nav-item"
+            }
+            onClick={() => onNavItemClicked("Statistics")}
+          >
+            Statistics
+          </div>
         </Link>
       </Nav>
       <Main>
--- frontend/src/layouts/MainLayout/styles.ts
+++ frontend/src/layouts/MainLayout/styles.ts
@@ -2,19 +2,39 @@ import styled from "styled-components";
 
 export const ButtonBox = styled.div`
   width: 25vw;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+
+  .search {
+    width: 24px;
+    height: 24px;
+    background: gray;
+  }
+
+  .hamberger-menu {
+    width: 24px;
+    height: 24px;
+    background: gray;
+    margin-left: 12px;
+  }
 `;
 
 export const Nav = styled.nav`
   display: flex;
   flex-direction: row;
-
   padding: 0 16px;
   background: #f7f7f7;
+
+  font-family: Pretendard;
+  font-size: 16px;
+  line-height: 19px;
+  letter-spacing: -0.2px;
 
   .nav-item {
     padding: 12px 10px;
   }
-  .stayed {
+  .current {
     font-weight: bold;
     border-bottom: 2px solid #838383;
   }
