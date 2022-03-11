6a2db78e36375abbc5cae05bf67499be14ff1225
jeonghye-choi <ppllhm.0@gmail.com>
2022-03-12 05:05:22 +0900

    feat: add `Google Login` request and success

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,48 +1,37 @@
 import * as React from "react";
-<<<<<<< HEAD
-import { GoogleLogin } from "react-google-login";
-
-import { LoginSection, Logo, LoginList, GoogleLoginButton } from "./styles";
-
-function Login() {
-  const onResponseGoogleSuccess = async (response: { code: string }) => {
-    const { code } = response;
-
-=======
 import { useCallback } from "react";
 import axios from "axios";
 import { DASONI_BACKEND_API, OAuthClientId } from "../../secret";
 import { GoogleLogin } from "react-google-login";
 
-// import { LoginSection, Logo, LoginList } from "./styles";
+import { LoginSection, Logo, LoginList, GoogleLoginButton } from "./styles";
+import { Navigate } from "react-router-dom";
 
 function Login() {
   const onResponseGoogleSuccess = async (response: any) => {
     const { code } = response;
 
-    console.log(code);
+    const body = new URLSearchParams({
+      code,
+      redirect_uri: "http://localhost:3000",
+    });
+
+    const options = {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Accept: "*/*",
+      },
+    };
 
     await axios
-      .post(
-        `${DASONI_BACKEND_API}/oauth2/login/google`,
-        {
-          code,
-          redirect_uri: "http://localhost:3000",
-        },
-        {
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Accept-Encoding": "gzip, deflate, br",
-          },
-        }
-      )
+      .post(`${DASONI_BACKEND_API}/oauth2/login/google`, body, options)
       .then((res) => {
         console.log(res);
+        <Navigate to="/" />;
       })
       .catch((error) => {
         console.log(error.response);
       });
->>>>>>> test
   };
 
   const onResponseGoogleFailure = useCallback((response) => {
@@ -50,7 +39,6 @@ function Login() {
   }, []);
 
   return (
-<<<<<<< HEAD
     <LoginSection>
       <Logo>
         <img src="" alt="dasoni-logo" />
@@ -59,37 +47,23 @@ function Login() {
         <GoogleLogin
           clientId={OAuthClientId}
           responseType="code"
-          scope="https://www.googleapis.com/auth/userinfo.email"
+          accessType="offline"
           buttonText="Register with Google"
-          onSuccess={onResponseGoogleSuccess}
           onFailure={onResponseGoogleFailure}
+          onSuccess={onResponseGoogleSuccess}
           redirectUri="http://localhost:3000"
+          render={(renderProps) => (
+            <GoogleLoginButton
+              onClick={renderProps.onClick}
+              disabled={renderProps.disabled}
+            >
+              <img src="" alt="google-logo" />
+              <span>Register with Google</span>
+            </GoogleLoginButton>
+          )}
         />
-          <span>Register with Google</span>
-        </GoogleLoginButton>
       </LoginList>
     </LoginSection>
-=======
-    // <LoginSection>
-    //   <Logo>
-    //     <img src="" alt="dasoni-logo" />
-    //   </Logo>
-    //   <LoginList>
-    <div>
-      <GoogleLogin
-        clientId={OAuthClientId}
-        responseType="code"
-        accessType="offline"
-        buttonText="Register with Google"
-        onFailure={onResponseGoogleFailure}
-        onSuccess={onResponseGoogleSuccess}
-        redirectUri="http://localhost:3000"
-      />
-    </div>
-
-    //   </LoginList>
-    // </LoginSection>
->>>>>>> test
   );
 }
 
