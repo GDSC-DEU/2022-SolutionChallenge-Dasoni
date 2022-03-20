95a4d6b18741e40bef9088403b5a3ca2cad2b0e5
jeonghye-choi <ppllhm.0@gmail.com>
2022-03-21 01:28:34 +0900

    feat: add `Support` page template

--- frontend/src/pages/Support/index.tsx
+++ frontend/src/pages/Support/index.tsx
@@ -1,7 +1,24 @@
+import SuppportContentBox from "components/molecules/boxes/SuppportContentBox";
+import ToggleBox from "components/molecules/boxes/ToggleBox";
 import * as React from "react";
+import { Title, ListWrap } from "./styles";
 
 function Support() {
-  return <>Support Page</>;
+  return (
+    <>
+      <Title>Welfare Assistance</Title>
+      <ListWrap>
+        <div className="filter">
+          <ToggleBox />
+        </div>
+        <div className="list">
+          <SuppportContentBox />
+          <SuppportContentBox />
+          <SuppportContentBox />
+        </div>
+      </ListWrap>
+    </>
+  );
 }
 
 export default Support;
