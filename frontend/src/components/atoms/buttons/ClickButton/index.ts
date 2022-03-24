dd8a3ccab94b4046a687b7a94376edc64a29d315
jeonghye-choi <ppllhm.0@gmail.com>
2022-03-25 00:50:45 +0900

    style: apply changed `signup page` design

--- frontend/src/components/atoms/buttons/ClickButton/index.ts
+++ frontend/src/components/atoms/buttons/ClickButton/index.ts
@@ -3,30 +3,34 @@ import styled from "styled-components";
 
 interface buttonProps {
   onClick: MouseEventHandler<HTMLButtonElement>;
+  kind: "solid" | "line";
   clicked?: boolean;
 }
 
 export const ClickButton = styled.button<buttonProps>`
   width: 100%;
   padding: 16px 0;
-  border: 1px solid #dadada;
   border-radius: 8px;
   text-align: center;
-  background: none;
-
   font-family: "Roboto";
   font-weight: 400;
-  font-size: 14px;
-  line-height: 16px;
-  color: #838383;
+  line-height: 112%;
   cursor: pointer;
 
   ${(props) =>
-    props.clicked && "background: #fe6f5b; font-weight: 700; color: white;"}
+    props.kind === "line"
+      ? "border: 1px solid #dadada; background: none; color: #838383;"
+      : "background: #DADADA; color: #FFFFFF; font-weight: 700; border:none;"}
+
+  ${(props) =>
+    props.clicked &&
+    props.kind === "line" &&
+    "border: 1px solid #FE6F5B; color: #FE6F5B;"}
 
   &:hover {
-    background: #fe6f5b;
-    font-weight: 700;
-    color: white;
+    ${(props) =>
+      props.kind === "line"
+        ? "border: 1px solid #fe6f5b; color: #fe6f5b;"
+        : "background: #FE6F5B;"}
   }
 `;
