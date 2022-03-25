96d54ec5d3eae84dbfbfe46cd6553782ea277e0f
jiy00nn <wldbs204@gmail.com>
2022-03-26 01:50:11 +0900

    DAS-98 feature: add entity for feed

--- backend/src/main/java/com/gdsc/backend/entity/Feed.java
+++ backend/src/main/java/com/gdsc/backend/entity/Feed.java
@@ -3,17 +3,18 @@ package com.gdsc.backend.entity;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.UUID;
+import java.util.List;
 
 @Schema(description = "피드")
 @Getter
 @Entity
 @Table(name = "feed")
 @NoArgsConstructor
-public class Feed {
+public class Feed extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Schema(description = "피드 아이디", nullable = false)
@@ -22,6 +23,10 @@ public class Feed {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "diary_id")
     private Diary diary;
+
+    @OneToMany
+    @JoinColumn(name = "feed_id")
+    private List<Comment> comments = new ArrayList<>();
 
     public Feed(Diary diary) {
         this.diary = diary;
