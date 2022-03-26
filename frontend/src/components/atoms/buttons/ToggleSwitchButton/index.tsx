f295d01a2df29421e83e662e3a2bd7930dda8f00
jeonghye-choi <ppllhm.0@gmail.com>
2022-03-26 16:31:51 +0900

    feat: add Warning sign depend on emotion(`sad`, `very sad`)

--- frontend/src/components/atoms/buttons/ToggleSwitchButton/index.tsx
+++ frontend/src/components/atoms/buttons/ToggleSwitchButton/index.tsx
@@ -1,15 +1,24 @@
 import * as React from "react";
 import { ToggleSwitchButtonWrap, ToogleBox, ToggleBoxLabel } from "./styles";
 
-interface ToggleSwitchButtonProps {}
+interface ToggleSwitchButtonProps {
+  checked: boolean;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
 
 function ToggleSwitchButton(props: ToggleSwitchButtonProps) {
+  const { checked, onChange } = props;
   // const { checked, onChange, className, label, labelClassName, ...rest } = props;
 
   return (
     <>
       <ToggleSwitchButtonWrap>
-        <ToogleBox id="toggle-switch-button" type="checkbox" />
+        <ToogleBox
+          id="toggle-switch-button"
+          type="checkbox"
+          checked={checked}
+          onChange={onChange}
+        />
         <ToggleBoxLabel htmlFor="toggle-switch-button" />
       </ToggleSwitchButtonWrap>
     </>
