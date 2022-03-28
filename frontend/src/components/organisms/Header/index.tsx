45d07d5bb0179ad66177d08d9aaf13267110625f
jeonghye-choi <ppllhm.0@gmail.com>
2022-03-28 23:40:52 +0900

    feat: add `SideBar` for nav

--- frontend/src/components/organisms/Header/index.tsx
+++ frontend/src/components/organisms/Header/index.tsx
@@ -2,12 +2,15 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 
 import { HeaderWrap, ButtonBox } from "./styles";
+import SlideBar from "components/organisms/navbars/SlideBar";
 
 import location from "assets/icons/location.png";
 import hamburger_menu from "assets/icons/hamburger_menu.png";
 import dasoni_logo_letter from "assets/logo/dasoni_logo_letter.svg";
 
 function Header(props: { position?: string }) {
+  const [display, setDisplay] = React.useState(false);
+
   return (
     <HeaderWrap position={props.position}>
       <div className="logo">
@@ -18,9 +21,10 @@ function Header(props: { position?: string }) {
       {props.position !== "middle" && (
         <ButtonBox>
           <img src={location} />
-          <img src={hamburger_menu} />
+          <img onClick={() => setDisplay(!display)} src={hamburger_menu} />
         </ButtonBox>
       )}
+      <SlideBar display={display} />
     </HeaderWrap>
   );
 }
