2b5c1a1006635299b823d14e3247fd5d05e3727d
jeonghye-choi <ppllhm.0@gmail.com>
2022-03-29 21:48:11 +0900

    feat: add `Feed` page structures and styles

--- frontend/src/pages/Feed/index.tsx
+++ frontend/src/pages/Feed/index.tsx
@@ -1,7 +1,34 @@
+import CheckBox from "components/atoms/inputs/CheckBox";
+import CalloutBox from "components/molecules/boxes/CalloutBox";
+import FeedContentBox from "components/molecules/boxes/FeedContentBox";
+import PageNav from "components/organisms/navbars/PageNav";
 import * as React from "react";
 
+import { FeedWrap, TodaySharedBox } from "./styles";
+
 function Feed() {
-  return <>Feed Page</>;
+  return (
+    <>
+      <PageNav currentPage="Feed" />
+      <FeedWrap>
+        <CalloutBox>
+          You can see others’ stories here. Express your sympathy with the heart
+          icon or replies.
+        </CalloutBox>
+        <TodaySharedBox>
+          <div className="title">
+            <span>Shared Today</span>
+            <CheckBox checkboxId="feed-show-mine-checkbox">
+              Show my diaries
+            </CheckBox>
+          </div>
+          <FeedContentBox />
+          <FeedContentBox />
+          <FeedContentBox />
+        </TodaySharedBox>
+      </FeedWrap>
+    </>
+  );
 }
 
 export default Feed;
---
+++ frontend/src/pages/Feed/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const FeedWrap = styled.article`
+  margin-top: 100px;
+  padding: 16px;
+`;
+
+export const TodaySharedBox = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  .title {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 6px 0;
+    justify-content: space-between;
+    margin-bottom: 16px;
+  }
+`;
