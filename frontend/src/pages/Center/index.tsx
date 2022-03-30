82b816915e133a9979271bfd498174c1dd17b70c
jeonghye-choi <ppllhm.0@gmail.com>
2022-03-31 02:57:30 +0900

    feat: add `Center` page stuctures and styles

--- frontend/src/pages/Center/index.tsx
+++ frontend/src/pages/Center/index.tsx
@@ -4,16 +4,28 @@ import { useEffect, useState } from "react";
 import { Input } from "components/atoms/inputs/Input";
 import Header from "components/organisms/Header";
 import Map from "components/organisms/Map";
-import * as React from "react";
+import useCenterActions from "hooks/useCenterActions";
 
-import { CenterWrap, Search, MapWrap } from "./styles";
+import indicator from "assets/icons/indicator.png";
+
+import { CenterWrap, Search, MapWrap, CenterListWrap } from "./styles";
+import { centersAtoms, CenterTypes } from "recoil/Center";
+import { useRecoilState } from "recoil";
+import CenterContentBox from "components/molecules/boxes/CenterContentBox";
 
 function Center() {
+  const centerActions = useCenterActions();
+  const [centers, setCenters] = useRecoilState<CenterTypes[]>(centersAtoms);
   const [searchRegion, setSearchRegion] = useState("Seoul");
   const positions = centers.map((center) => ({
     lat: center.latitude,
     lng: center.longitude,
   }));
+
+  useEffect(() => {
+    centerActions.getCenters();
+  }, []);
+
   return (
     <>
       <Header />
@@ -29,6 +41,19 @@ function Center() {
         <MapWrap>
           <Map positions={positions} searchRegion={searchRegion} />
         </MapWrap>
+        <CenterListWrap>
+          <div className="scroll-box">
+            <div className="title">
+              <img src={indicator} />
+              <span>Click to Show List</span>
+            </div>
+            <div className="list-wrap">
+              {centers.map((center, index) => (
+                <CenterContentBox key={index} information={center} />
+              ))}
+            </div>
+          </div>
+        </CenterListWrap>
       </CenterWrap>
     </>
   );
--- frontend/src/pages/Center/styles.ts
+++ frontend/src/pages/Center/styles.ts
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 
 export const CenterWrap = styled.div`
+  height: calc(100vh - 56px);
   margin-top: 56px;
+  overflow: hidden;
 `;
 
 export const Search = styled.section`
@@ -11,4 +13,43 @@ export const Search = styled.section`
 
 export const MapWrap = styled.section`
   height: calc(100vh - 120px);
+`;
+
+export const CenterListWrap = styled.section`
+  position: absolute;
+  z-index: 100;
+  top: 50vh;
+  width: 100%;
+  height: 50vh;
+  overflow: scroll;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  background: #ffffff;
+  border-radius: 12px 12px 0px 0px;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+
+  .title {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    font-family: "Roboto";
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 150%;
+    margin-bottom: 5px;
+    cursor: pointer;
+    color: #9b9b9b;
+
+    img {
+      width: 24px;
+      height: 24px;
+    }
+  }
+
+  .list-wrap {
+  }
 `;
