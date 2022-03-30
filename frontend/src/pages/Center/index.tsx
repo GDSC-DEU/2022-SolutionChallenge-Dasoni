414c5a65a474b156aa4dccba52a2f5fb167fe9ec
jeonghye-choi <ppllhm.0@gmail.com>
2022-03-31 02:56:47 +0900

    feat: add Search region function

--- frontend/src/pages/Center/index.tsx
+++ frontend/src/pages/Center/index.tsx
@@ -1,3 +1,6 @@
+import * as React from "react";
+import { useEffect, useState } from "react";
+
 import { Input } from "components/atoms/inputs/Input";
 import Header from "components/organisms/Header";
 import Map from "components/organisms/Map";
@@ -6,12 +9,22 @@ import * as React from "react";
 import { CenterWrap, Search, MapWrap } from "./styles";
 
 function Center() {
+  const [searchRegion, setSearchRegion] = useState("Seoul");
+  const positions = centers.map((center) => ({
+    lat: center.latitude,
+    lng: center.longitude,
+  }));
   return (
     <>
       <Header />
       <CenterWrap>
         <Search>
-          <Input />
+          <Input
+            type="text"
+            value={searchRegion}
+            onChange={(e) => setSearchRegion(e.target.value)}
+            placeholder="Find a facility for single parent"
+          />
         </Search>
         <MapWrap>
           <Map positions={positions} searchRegion={searchRegion} />
