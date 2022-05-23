ee54a9c9b5e47902615746701a8be177a9b4e9bb
jeonghye-choi <ppllhm.0@gmail.com>
2022-05-23 11:56:23 +0900

    feat: add `FeedDetail` page

---
+++ frontend/src/pages/FeedDetail/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const FeedDetailWrap = styled.article`
+  padding: 16px;
+`;
+
+export const FeedDetailContent = styled.section`
+  padding: 16px;
+`;
